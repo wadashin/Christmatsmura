2bdf51a46b82e4a77e86c07e2314a0fafe841ba9
Ryuki <liuyijichuan@gmail.com>
2021-12-22 13:38:59 +0900

    ps

--- Assets/Script/OikawaRyuki/Script/Present.cs
+++ Assets/Script/OikawaRyuki/Script/Present.cs
@@ -7,26 +7,38 @@ public class Present : MonoBehaviour
 {
     [Header("落ちる速度")]public float _fallSpeed;
     [Header("消える位置")]public float _destroyPos;
-    [Header("プレゼント")] public GameObject _present;
+    [Header("プレゼント")]public GameObject _present;
 
     private GameObject _player;
     private Vector3 _presentScale;
     private Vector3 _playerPos;
+    private Rigidbody rb;
     private void Start()
     {
         _player = GameObject.Find("Player");
+        rb = this.GetComponent<Rigidbody>();
         _playerPos = _player.transform.position + Vector3.down;
-        _presentScale = this.transform.localScale;
+        _presentScale = new Vector3(1, 1, 1);
         ScaleChange();
     }
 
     private void FixedUpdate()
     {
-        Rigidbody rb = this.GetComponent<Rigidbody>();
         rb.AddForce(transform.up * -_fallSpeed, ForceMode.Force);
-
+        print(transform.localScale);
         if (this.transform.position.y <= _destroyPos)
         {
+            this.gameObject.transform.localScale = _presentScale;
+            this.gameObject.transform.position = _playerPos;
+            rb.velocity = Vector3.zero;
+            ScaleChange();
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("House"))
+        {
+            other.transform.DOScale(1.5f, 0.2f);
             this.gameObject.transform.localScale = _presentScale;
             this.gameObject.transform.position = _playerPos;
             rb.velocity = Vector3.zero;
