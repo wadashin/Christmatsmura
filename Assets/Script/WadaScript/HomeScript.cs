1cd257a0272f3c67d73d34a761f1b57358a2a698
wadashin <Shin.86.miso@outlook.com>
2021-12-22 15:55:02 +0900

    田中が増す村になった。元気なおかまの子です。

--- Assets/Script/WadaScript/HomeScript.cs
+++ Assets/Script/WadaScript/HomeScript.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeScript : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float _Posi;
+    [SerializeField] Text _MuraName;
+    string a = "クリス増す村";
+    string b = "クリス増す村";
+    string[] strArray = new string[5] { "クリス増す村", "リスク増す村", "クスリ増す村", "田中増す村", "佐藤増す村" };
     void Start()
     {
         
@@ -18,9 +23,12 @@ public class HomeScript : MonoBehaviour
     }
     public void HouseInstasiate()
     {
+        int i = Random.Range(0,5);
+        _MuraName.text = strArray[i];
         var x = this.gameObject;
         var y = Instantiate(x);
         y.transform.position = new Vector3(x.transform.position.x, x.transform.position.y, x.transform.position.z - 180);
+        y.transform.parent = GameObject.Find("Phase1").transform;
     }
     public void HomeDes()
     {
