c8bc2d330e23e62d5e1ed69babf5ee8517898bab
Ryuki <liuyijichuan@gmail.com>
2021-12-22 12:43:48 +0900

    Title進捗途中

---
+++ Assets/Script/OikawaRyuki/Script/PlayerJump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float Bottom;
+    private Rigidbody rb;
+    private void Awake()
+    {
+       rb = this.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (transform.position.y < Bottom)
+        {
+            AutoJump();
+        }
+    }
+    private void AutoJump()//最下部で自動ジャンプ
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(transform.up * 10, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Script/OikawaRyuki/Script/Present.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    [Header("落ちる速度")]public float _fallSpeed;
+    [Header("消える位置")]public float _destroyPos;
+    [Header("プレゼント")] public GameObject _present;
+
+    private GameObject _player;
+    private Vector3 _playerPos;
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+        _playerPos = _player.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * -_fallSpeed, ForceMode.Force);
+
+        if (this.transform.position.y <= _destroyPos)
+        {
+            Crate();
+            Destroy(this.gameObject);
+        }
+    }
+    private void Crate()
+    {
+        Instantiate(_present, _playerPos, Quaternion.identity);
+    }
+}
